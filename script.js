42d46dc11bc70fe6e8d3d7357bce17aef78d9bbd
mlichte2 <58532755+mlichte2@users.noreply.github.com>
2021-02-05 12:38:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+<script defer type="text/javascript">
+    (function(){
+        var t = document.getElementsByTagName('script')[0];
+        var s = document.createElement('script'); s.async = true;
+        s.src = '//widget.nfusionsolutions.com/custom/monex/script/chart/1/a0fa8f6f-0b7b-4d1a-bb3f-045d29d8aee5/475a1e90-7ecc-4a36-a85b-ef15136924bb';
+        t.parentNode.insertBefore(s, t);
+    })();
+</script>
