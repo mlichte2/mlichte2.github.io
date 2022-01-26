5e8e9ea6cdd0e0955aaead45c1cef102c1e55337
Michael <lichtenberger.dev@gmail.com>
2022-01-26 17:53:21 -0600

    added dark mode

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("No javascript yet...");
+const toggleButton = document.querySelector("#dark-mode-button");
+const body = document.querySelector("body");
+const contentSection = document.querySelectorAll(".content-section");
+const links = document.querySelectorAll("a");
+
+let buttonValue = true;
+
+toggleButton.addEventListener("click", () => {
+  toggleLightDarkMode();
+});
+
+const toggleLightDarkMode = () => {
+  body.classList.toggle("dark-mode");
+  contentSection.forEach((element) => {
+    element.classList.toggle("dark-mode");
+  });
+  links.forEach((element) => {
+    element.style.setProperty("color", "orange");
+  });
+  toggleButton.classList.toggle("button-mode-light");
+
+  function switchText() {
+    if (buttonValue === false) {
+      toggleButton.innerText = "Light Mode 🔆";
+    } else {
+      toggleButton.innerText = "Dark Mode 🌝";
+    }
+  }
+  buttonValue = !buttonValue;
+  switchText();
+};
